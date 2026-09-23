c84e0617476fd7c2702a2305fd3d920b8b0b544d
kaung139abc-hash <kaung139abc@gmail.com>
2026-09-23 20:54:54 +0700

    Add new standalone 3D Live Wallpaper project

---
+++ 3DLiveWallpaper/app/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android { namespace="com.kaung.threedwallpaper"; compileSdk=35
+    defaultConfig { applicationId="com.kaung.threedwallpaper"; minSdk=26; targetSdk=35; versionCode=1; versionName="1.0" }
+}
