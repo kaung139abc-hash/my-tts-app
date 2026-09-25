2ef54a308752a4a42d934d9bf9774cc16c3ef4d6
kaung139abc-hash <kaung139abc@gmail.com>
2026-09-25 14:30:57 +0700

    Use foreground-service start API on Android 8+ and prevent duplicate starts

--- HyperOSTaskbar/app/src/main/java/com/kaunggyi/hyperostaskbar/MainActivity.java
+++ HyperOSTaskbar/app/src/main/java/com/kaunggyi/hyperostaskbar/MainActivity.java
@@ -3,6 +3,7 @@ package com.kaunggyi.hyperostaskbar;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.widget.Button;
@@ -10,11 +11,21 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private void startTaskbar() {
+        Intent intent = new Intent(this, TaskbarService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent);
+        } else {
+            startService(intent);
+        }
+    }
+
     @Override public void onCreate(Bundle b) {
         super.onCreate(b);
+
         LinearLayout box = new LinearLayout(this);
         box.setOrientation(LinearLayout.VERTICAL);
-        box.setPadding(48,48,48,48);
+        box.setPadding(48, 48, 48, 48);
 
         TextView title = new TextView(this);
         title.setText("HyperOS Taskbar\n\nFloating taskbar for quick app launching");
@@ -34,7 +45,10 @@ public class MainActivity extends Activity {
         start.setText("Start Taskbar");
         start.setOnClickListener(v -> {
             if (Settings.canDrawOverlays(this)) {
-                startService(new Intent(this, TaskbarService.class));
+                startTaskbar();
+            } else {
+                startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName())));
             }
         });
         box.addView(start);
